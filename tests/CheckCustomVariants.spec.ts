65c13091c70382f998b4d0bffc254a0e48e1c4a8
im <imartemy1524@gmail.com>
2024-09-01 03:36:02 +0200

    Add unit tests #2

--- tests/CheckCustomVariants.spec.ts
+++ tests/CheckCustomVariants.spec.ts
@@ -1,9 +1,19 @@
+import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { toNano } from '@ton/core';
+import { Move, TactChess } from '../wrappers/TactChess';
 import '@ton/test-utils';
-import { describe } from 'node:test';
-import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { TactChess } from '../build/TactChess/tact_TactChess';
+import {
+    buildChessBoardFromGame,
+    checkGame,
+    chessMoveToBlockchain,
+    defaultChess,
+    expectEqualsChess,
+    expectSuccessMove,
+    toGame,
+} from './_chess';
+import { Chess } from 'chess.js';
 import { deployChess } from './_tests';
-import { checkGame, toGame } from './_chess';
+
 
 describe('CheckCustomVariants', () => {
     let blockchain: Blockchain;
@@ -15,4 +25,6 @@ describe('CheckCustomVariants', () => {
     });
 
     it('check en passant', () => checkGame(chess, deployer.getSender(), toGame(`1. a3 d5 2. a4 d4 3. e4 dxe3`)));
+    it('check castling', () => checkGame(chess, deployer.getSender(), toGame(`1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. O-O axb5`)));
+    it('check queen castling', () => checkGame(chess, deployer.getSender(), toGame(`1. d4 d5 2. Be3 Be6 3. Qd3 Qd6 3. Nc3 Nc6 4. O-O-O O-O-O`)));
 });
